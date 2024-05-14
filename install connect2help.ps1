33bb33ef062cf86aa6ada59fbf894684ef43eb0f
gshanks-prepnetworks <167108700+gshanks-prepnetworks@users.noreply.github.com>
2024-05-14 14:10:31 -0500

    Create install connect2help.ps1

---
+++ install connect2help.ps1
@@ -0,0 +1,3 @@
+﻿$installerpath = "C:\temp\Connect2HelpDotNet.msi"
+Invoke-WebRequest -uri https://github.com/gshanks-prepnetworks/Connect2Help/raw/main/Connect2HelpDotNet.msi -OutFile $installerpath
+Start-Process msiexec.exe -ArgumentList "/i $installerpath /qn /norestart"
